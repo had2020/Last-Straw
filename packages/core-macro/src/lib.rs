03341731e299aec46bc01d159c0319e7329a9664
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-12-29 00:33:34 -0800

    updates

--- packages/core-macro/src/lib.rs
+++ packages/core-macro/src/lib.rs
@@ -9,9 +9,7 @@ pub fn asx(input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         {
-            let _generated_block_wrapper = || {
-                let (mut window, events) = defined_window(true, 500, 500, "test")
-                    .expect("Failed to Define window!");
+            let mut _generated_block_wrapper = || {
                 
                 let mut err_object = error_init(); 
 
@@ -21,15 +19,18 @@ pub fn asx(input: TokenStream) -> TokenStream {
                     // input handling
                     err_object.poll_events();
 
+                    // input key
                     for (_, event) in glfw::flush_messages(&events) {
                         match event {
                             glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => { 
                                 window.set_should_close(true)
+                                
                             },
                             _ => {},
                         }
                     }
                     /* _ */
+
                     #block
                 }
             };
--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -27,8 +27,8 @@ use glfw::{fail_on_errors, Action, Context, Glfw, GlfwReceiver, MouseButton, PWi
 pub fn defined_window( resizeable: bool, width: u32, height: u32, name: &str) -> Option<(glfw::PWindow, glfw::GlfwReceiver<(f64, glfw::WindowEvent)>)> {
     let mut glfw = glfw::init(fail_on_errors).expect("Failed to initialize GLFW");
 
-    let (mut window, events) = glfw.create_window(width, height, name, glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window, with defined_window method.");
+    let (mut window, events) = glfw.create_window(width, height, name, glfw::WindowMode::Windowed).unwrap();
+        //.expect("Failed to create GLFW window, with defined_window method.");
 
     window.make_current();
     window.set_key_polling(true);
