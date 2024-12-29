d73153ef6fbce33ed5111fa00af01d0ec7e0b356
Hadrian Lazic <59424667+had2020@users.noreply.github.com>
2024-12-28 23:24:26 -0800

    updates

--- packages/core-macro/src/lib.rs
+++ packages/core-macro/src/lib.rs
@@ -3,41 +3,38 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, Block};
 
-//use glfw::{fail_on_errors, Action, Context, Glfw, GlfwReceiver, MouseButton, PWindow, WindowEvent, Key}; // new argument for type of input maybe a way to have multiple inputs
-
 #[proc_macro]
 pub fn asx(input: TokenStream) -> TokenStream {
     let block = parse_macro_input!(input as Block);
 
     let expanded = quote! {
-        fn _generated_block_wrapper() {
-
-            let (mut window, events) = defined_window(true, 500, 500, "test") // need to figure out how to pass it from outside MAIN TODO!!!
-                .expect("Failed to Define window!");
-            
-            let mut err_object = error_init(); 
-
-            while !window.should_close() { 
-                window.swap_buffers();
+        {
+            let _generated_block_wrapper = || {
+                let (mut window, events) = defined_window(true, 500, 500, "test")
+                    .expect("Failed to Define window!");
                 
-                //input_handling(window, events, err_object); // failing
-                // input handling
-                err_object.poll_events();
+                let mut err_object = error_init(); 
 
-                for (_, event) in glfw::flush_messages(&events) {
-                    //println!("{:?}", event); // debug
-                    match event { // asx! macro will be used to replace this
-                        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => { 
-                            window.set_should_close(true)
-                        },
-                        _ => {},
+                while !window.should_close() { 
+                    window.swap_buffers();
+                    
+                    // input handling
+                    err_object.poll_events();
+
+                    for (_, event) in glfw::flush_messages(&events) {
+                        match event {
+                            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => { 
+                                window.set_should_close(true)
+                            },
+                            _ => {},
+                        }
                     }
+                    /* _ */
+                    #block
                 }
-                // end of input handling
-                #block
-            }
+            };
+            _generated_block_wrapper();
         }
-        _generated_block_wrapper();
     };
 
     TokenStream::from(expanded)
